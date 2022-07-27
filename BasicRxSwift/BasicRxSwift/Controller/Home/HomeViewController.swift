d8f189d9d0388d227e5d2ac1ec594dc754731b83
Thien Tam <nguyenkhanhthientam@gmail.com>
2022-07-27 11:49:23 +0700

    init

---
+++ BasicRxSwift/BasicRxSwift/Controller/Home/HomeViewController.swift
@@ -0,0 +1,15 @@
+//
+//  HomeViewController.swift
+//  BasicRxSwift
+//
+//  Created by Tam Nguyen K.T. [7] VN.Danang on 7/27/22.
+//
+
+import UIKit
+
+final class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
